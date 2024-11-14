3a9bbc1e519e9dfe27176a24c4ce8774582db642
Nathan Patterson <nathan.e.patterson@gmail.com>
2024-11-14 10:57:02 -0700

    Fix wallet login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,8 +9,10 @@ import { useRouter } from "next/navigation";
 import { signIn, getCsrfToken } from "next-auth/react";
 import { toast } from "react-toastify";
 import { useAccount, useDisconnect } from "wagmi";
+
 import { StandardCredentials, EthereumCredentials } from "@/auth/Credentials";
 import { LoginPreference } from "@/utils/Constants";
+
 import Header from "../components/global/auth_header";
 
 const Credentials = [StandardCredentials, EthereumCredentials];
@@ -53,7 +55,8 @@ const Login = function ({ csrfToken, callbackUrl }: InferGetServerSidePropsType<
     if (ethereumAccount.address) {
       handleWalletDisconnect();
     }
-  }, [disconnect, ethereumAccount.address]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Layout
