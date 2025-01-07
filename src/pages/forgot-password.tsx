3a6d5d679cdc329ff04e7b527ab4fba0e382d56b
Justina <justinamary27@gmail.com>
2025-01-07 09:54:11 +0530

    fixed build issue

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -6,10 +6,11 @@ import { Layout } from "antd";
 import { Form, Input, Card, Radio, Button } from "antd/es";
 import Image from "next/image";
 import Link from "next/link"; // Import Link from next/link
+import { useRouter } from "next/navigation";
 import Parse from "parse";
 import { SubmitHandler } from "react-hook-form";
 import { toast } from "react-toastify";
-import { useRouter } from "next/navigation";
+
 import Header from "../components/global/auth_header";
 
 type ForgotPasswordFormInputs = {
