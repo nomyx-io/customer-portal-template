702815fcc327d9e13d551a23538aecf526094b0b
hashdive <nile.sarkisian@gmail.com>
2025-02-14 11:50:22 -0800

    updated reg handler/ app context for byob onboard flow debug. The issue was that the reg handler was expecting the recovery key, but byob doesn't have one (only dfns does). Also, I updated the gemforce app context so that it doesn't try to log in when the user connects via the /onboarding page

--- src/pages/api/submitRegistration.ts
+++ src/pages/api/submitRegistration.ts
@@ -3,6 +3,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Parse from "parse/node";
 
+import { WalletPreference } from "@/utils/Constants";
+
 // Initialize Parse
 Parse.initialize(process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID || "", process.env.NEXT_PUBLIC_PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = `${process.env.NEXT_PUBLIC_PARSE_SERVER_URL}/parse`;
@@ -14,25 +16,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const { email, registration, recoveryKey } = req.body;
 
-  if (!email || !registration || !recoveryKey) {
-    return res.status(400).json({ message: "Invalid data" });
+  if (!email || !registration) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  // Check if recoveryKey is required based on wallet preference
+  if (registration.walletPreference === WalletPreference.MANAGED && !recoveryKey) {
+    return res.status(400).json({ message: "Recovery key required for managed wallets" });
   }
 
   try {
-    const secretKey = process.env.AUTH_SECRET_KEY; // Server-side access only
+    const secretKey = process.env.AUTH_SECRET_KEY;
 
-    // Update user by email using Parse Cloud function, including recoveryKey
+    // Update user by email using Parse Cloud function
     const response = await Parse.Cloud.run("updateUserByEmail", {
       email,
       updates: registration,
       secretKey,
+      recoveryKey: recoveryKey || null, // Pass null if no recovery key
     });
 
-    // Check for expected response structure and handle errors properly
     if (response?.message) {
       return res.status(200).json({
         message: "User onboarded successfully",
-        data: response, // Pass the response data back to the client
+        data: response,
       });
     } else {
       return res.status(500).json({ message: response?.error || "Failed to onboard user" });
