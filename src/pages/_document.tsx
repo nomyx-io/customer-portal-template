3fb439e0132f49ecdcad1823e0920bf69b7b4da2
hashdive <nile.sarkisian@gmail.com>
2025-04-06 19:05:59 -0700

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,9 @@ import type { DocumentContext } from "next/document";
 
 const MyDocument = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <link rel="icon" href="/favicon.png" />
+    </Head>
     <body style={{ background: "black" }}>
       <Main />
       <NextScript />
