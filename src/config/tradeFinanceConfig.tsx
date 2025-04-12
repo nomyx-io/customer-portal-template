bb0d556a1df45fe7a36a4e796c0a7f1421178ba7
Mohammadreza Soltaninia <tech.mrsoltaninia@gmail.com>
2025-04-12 14:12:41 -0400

    add document fields to be rendered in token detail

--- src/config/tradeFinanceConfig.tsx
+++ src/config/tradeFinanceConfig.tsx
@@ -8,4 +8,22 @@ export const tradeFinanceFields = [
   //   placeHolder: "Enter Trade Amount",
   //   rules: [requiredRule, numberRule],
   // },
+  {
+    name: "ppm",
+    key: "ppm",
+    type: "text",
+    placeHolder: "ppm Document",
+  },
+  {
+    name: "Stock Certificate",
+    key: "stockCertificate",
+    type: "text",
+    placeHolder: "Stock Certificate Document",
+  },
+  {
+    name: "Custodian Contract",
+    key: "custodianContract",
+    type: "text",
+    placeHolder: "Custodian Contract Document",
+  },
 ];
