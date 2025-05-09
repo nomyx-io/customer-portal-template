4af1e67df7b27e2e0cf4c523edbba8101391663f
Justina <justinamary27@gmail.com>
2025-05-09 20:55:35 +0530

    added logic to fit the large values inside a container

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -437,13 +437,15 @@ const Dashboard: React.FC = () => {
                           title={<span className="text-nomyx-gray2-light dark:text-nomyx-gray2-dark">{stat.title}</span>}
                           value={stat.value}
                           formatter={() => (
-                            <div className="flex items-center space-x-2">
+                            <div className="flex items-center space-x-2 justify-center">
                               {stat.icon}
-                              <span className={stat.color}>{stat.value}</span>
+                              <span className={`${stat.color} truncate`}>{stat.value}</span>
                             </div>
                           )}
                           valueStyle={{
                             color: stat.color,
+                            width: "100%",
+                            height: "100%",
                             display: "flex",
                             justifyContent: "center",
                             alignItems: "center",
